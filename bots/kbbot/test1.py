4a7206ab95866d43131187868819747cd7c09c6f
MateuszBelka <belka.mateusz.bm@gmail.com>
2020-01-13 20:04:29 +0100

    convert kb to clause normal form and write script for it

--- bots/kbbot/test1.py
+++ bots/kbbot/test1.py
@@ -22,3 +22,28 @@ for model in kb.models():
 
 # Print out whether the KB is satisfiable (if there are no models, it is not satisfiable)
 print(kb.satisfiable())
+
+
+
+"""
+# Define our symbols
+A = Boolean('A')
+B = Boolean('B')
+C = Boolean('C')
+D = Boolean('D')
+
+# Create a new knowledge base
+kb = KB()
+
+kb.add_clause(~A, B)
+kb.add_clause(B, A)
+kb.add_clause(A, C)
+kb.add_clause(A, D)
+
+# Print all models of the knowledge base
+for model in kb.models():
+    print(model)
+
+# Print out whether the KB is satisfiable (if there are no models, it is not satisfiable)
+print(kb.satisfiable())
+"""
