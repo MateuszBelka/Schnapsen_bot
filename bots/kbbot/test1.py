59469c64fb57e32689196ce074be3cb067a9b5f6
MateuszBelka <belka.mateusz.bm@gmail.com>
2020-01-14 15:35:11 +0100

    kbbot Q5

--- bots/kbbot/test1.py
+++ bots/kbbot/test1.py
@@ -1,49 +1,77 @@
 import sys
 from kb import KB, Boolean, Integer, Constant
 
-"""
-# Define our symbols
-A = Boolean('A')
-B = Boolean('B')
-C = Boolean('C')
+exercise = 5
 
-# Create a new knowledge base
-kb = KB()
+if exercise == 0:
+    # Define our symbols
+    A = Boolean('A')
+    B = Boolean('B')
+    C = Boolean('C')
 
-# Add clauses
-kb.add_clause(A, B, C)
-kb.add_clause(~A, B)
-kb.add_clause(~B, C)
-kb.add_clause(B, ~C)
-kb.add_clause(~B, ~C) # My clause
+    # Create a new knowledge base
+    kb = KB()
 
-# Print all models of the knowledge base
-for model in kb.models():
-    print(model)
+    # Add clauses
+    kb.add_clause(A, B, C)
+    kb.add_clause(~A, B)
+    kb.add_clause(~B, C)
+    kb.add_clause(B, ~C)
+    kb.add_clause(~B, ~C) # My clause
 
-# Print out whether the KB is satisfiable (if there are no models, it is not satisfiable)
-print(kb.satisfiable())
-"""
+    # Print all models of the knowledge base
+    for model in kb.models():
+        print(model)
 
+    # Print out whether the KB is satisfiable (if there are no models, it is not satisfiable)
+    print(kb.satisfiable())
+elif exercise == 3:
+    # Define our symbols
+    A = Boolean('A')
+    B = Boolean('B')
+    C = Boolean('C')
+    D = Boolean('D')
 
+    # Create a new knowledge base
+    kb = KB()
 
-# Define our symbols
-A = Boolean('A')
-B = Boolean('B')
-C = Boolean('C')
-D = Boolean('D')
+    kb.add_clause(A, B)
+    kb.add_clause(~B, A)
+    kb.add_clause(~A, C)
+    kb.add_clause(~A, D)
+    kb.add_clause(~A) # Ex 4
+    kb.add_clause(~C) # Ex 4
+    kb.add_clause(~D) # Ex 4
 
-# Create a new knowledge base
-kb = KB()
+    # Print all models of the knowledge base
+    for model in kb.models():
+        print(model)
 
-kb.add_clause(A, B)
-kb.add_clause(~B, A)
-kb.add_clause(~A, C)
-kb.add_clause(~A, D)
+    # Print out whether the KB is satisfiable (if there are no models, it is not satisfiable)
+    print(kb.satisfiable())
+elif exercise == 5:
+    # Define our symbols
+    P = Boolean('P')
+    Q = Boolean('Q')
+    R = Boolean('R')
 
-# Print all models of the knowledge base
-for model in kb.models():
-    print(model)
+    # Create a new knowledge base
+    kb = KB()
 
-# Print out whether the KB is satisfiable (if there are no models, it is not satisfiable)
-print(kb.satisfiable())
+    # Add clauses
+    kb.add_clause(P,Q)
+    kb.add_clause(~Q,R)
+    kb.add_clause(~R,~P)
+    # ~P v ~Q ^ Q v P
+    kb.add_clause(~P,~Q) # entailed by
+    kb.add_clause(P,Q) # entailed by
+    # P v Q ^ Q v ~P
+    kb.add_clause(P,Q) # check unsatisfiability
+    kb.add_clause(~P,Q) # check unsatisfiability
+
+    # Print all models of the knowledge base
+    for model in kb.models():
+        print(model)
+
+    # Print out whether the KB is satisfiable (if there are no models, it is not satisfiable)
+    print(kb.satisfiable())
